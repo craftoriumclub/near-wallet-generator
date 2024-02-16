375be6009a173cf6ff54aad9d5a71f60b4389660
Dmytro Shashkevych <shashkevich41@gmail.com>
2024-02-16 14:58:38 +0200

    production in the testnet & changed link for the webpage

--- src/state/config.js
+++ src/state/config.js
@@ -12,20 +12,21 @@ let config = {
   walletUrl: 'https://testnet.mynearwallet.com/recover-seed-phrase',
   nameSuffix: '.craftorium.testnet',
   contractName: 'testnet',
-  craftoriumEcosystemWebSite: 'https://6575e690648ed7421817720a--clever-toffee-b6a7c6.netlify.app/',
+  craftoriumEcosystemWebSite: 'https://ibbclub.org/eco-frontend/',
 };
 
 if (process.env.REACT_APP_ENV === 'prod') {
   config = {
     ...config,
-    networkId: 'mainnet',
-    nodeUrl: 'https://rpc.mainnet.near.org',
-    walletUrlRecoverSeedPhrase: 'https://app.mynearwallet.com/recover-seed-phrase',
-    walletUrlRecoverPrivateKey: 'https://app.mynearwallet.com/recover-private-key',
-    walletUrl: 'https://app.mynearwallet.com/',
-    nameSuffix: '.near',
-    contractName: 'near',
-    exchangeWebSite: 'https://6575e690648ed7421817720a--clever-toffee-b6a7c6.netlify.app/',
+    networkId: 'testnet',
+    privateKeyMaster: 'ed25519:4d8mGUpPYPpY7hCPyAnkpBwosaVCX1qcuTkP2A2cV5pQZqbhV4T9geVN3gsUXTnaVY4jLSjEDQ1jD2V8SSYce2SS',
+    nodeUrl: 'https://rpc.testnet.near.org',
+    walletUrlRecoverSeedPhrase: 'https://testnet.mynearwallet.com/recover-seed-phrase',
+    walletUrlRecoverPrivateKey: 'https://testnet.mynearwallet.com/recover-private-key',
+    walletUrl: 'https://testnet.mynearwallet.com/recover-seed-phrase',
+    nameSuffix: '.craftorium.testnet',
+    contractName: 'testnet',
+    craftoriumEcosystemWebSite: 'https://ibbclub.org/eco-frontend/',
   };
 }
 
